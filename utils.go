7b6d12f01d41faf0dd10ece69f6d366bcf2d001a
Victor Lopez <victorcete@gmail.com>
2015-09-03 22:11:15 +0200

    added utils.go

---
+++ utils.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+// generic method to check if an element exists in a slice
+func contains(s []string, i string) bool {
+	for _, v := range s {
+		if v == i {
+			return true
+		}
+	}
+	return false
+}
+
+// custom type to store the comma-separated list of
+// redis ports to be monitored (in case there is more
+// than one instance on the host)
+type redisPorts []string
+
+// Set implementation for flag.Value interface
+func (i *redisPorts) Set(value string) error {
+	if len(*i) > 0 {
+		return errors.New("redis ports flag already set")
+	}
+	for _, val := range strings.Split(value, ",") {
+		if !contains(*i, val) {
+			*i = append(*i, val)
+		}
+	}
+	return nil
+}
+
+// String implementation for flag.Value interface
+func (i *redisPorts) String() string {
+	return fmt.Sprint(*i)
+}
