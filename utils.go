ca547b16004b0e7bfff9d31bad037d438c75780e
Victor Lopez <victorcete@gmail.com>
2015-11-15 15:54:25 +0100

    get rid of old code after autodiscovering instances

--- utils.go
+++ utils.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"errors"
-	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -35,39 +33,6 @@ func newPool(port string) *redis.Pool {
 func hostnameGraphite() string {
 	hostname, _ := os.Hostname()
 	return strings.Replace(hostname, ".", "_", -1)
-}
-
-// Generic method to check if an element exists in a slice.
-func contains(s []string, i string) bool {
-	for _, v := range s {
-		if v == i {
-			return true
-		}
-	}
-	return false
-}
-
-// Custom type to store the comma-separated list of
-// redis ports to be monitored (in case there is more
-// than one instance on the host).
-type redisPorts []string
-
-// Set implementation for flag.Value interface.
-func (i *redisPorts) Set(value string) error {
-	if len(*i) > 0 {
-		return errors.New("redis ports flag already set")
-	}
-	for _, v := range strings.Split(value, ",") {
-		if !contains(*i, v) {
-			*i = append(*i, v)
-		}
-	}
-	return nil
-}
-
-// String implementation for flag.Value interface.
-func (i *redisPorts) String() string {
-	return fmt.Sprint(*i)
 }
 
 func discoverInstances() []string {
